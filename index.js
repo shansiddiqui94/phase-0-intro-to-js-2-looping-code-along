c8f1b5dfea409a01cf8245f6be035ff92ca07eff
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-01-19 04:01:01 -0500

    complete looping

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(people, event){
+    let newArr = []
+        for(let i = 0; i < people.length; i++){
+            let messages = `Thank you, ${people[i]}, for the wonderful ${event} gift!`
+            newArr.push(messages)
+        }
+            return newArr 
+    }
+    
+    function countDown(int) {
+        while (int >= 0) {
+          console.log(int);
+          int -= 1;
+        }
+        return int;
+      }
